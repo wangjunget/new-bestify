96839116a1937da73a1e857b2ce5b0637df29a18
王俊 <wj@sss>
2020-06-24 18:40:38 +0800

    更新doc组件注册

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,11 +1,21 @@
 import Alert from '../../packages/alert'
 
+const components = [Alert]
+
+/**
+ * 注册全局组件
+ */
+function registerComponents(Vue) {
+  components.forEach((component) => {
+    Vue.component(component.name, component)
+  })
+}
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
-  siteData // 站点元数据
+  siteData, // 站点元数据
 }) => {
-  Vue.component(Alert.name, Alert);
+  registerComponents(Vue)
 }
