31a22ef6df0e0dc994984b45ae77379c5dcffd2b
hanjinbo <hanjinboo123@163.com>
2020-08-20 20:27:14 +0800

    feature: 更新image图片组件

---
+++ packages/image/src/image-preview.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nb-image-viewer__wrapper">
+    <div class="nb-image-viewer__mask"></div>
+    <span class="nb-image-viewer__btn nb-image-viewer__prev">
+      <i class="iconfont nbhoutui-copy"></i>
+    </span>
+    <span class="nb-image-viewer__btn nb-image-viewer__next">
+      <i class="iconfont nbhoutui-copy1"></i>
+    </span>
+    <span class="nb-image-viewer__btn nb-image-viewer__close">
+      <i class="iconfont nbguanbi"></i>
+    </span>
+    <div class="nb-image-viewer__content">
+      <img :src="previewList[index]" alt class="nb-image-viewer__img" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NbImageViewer",
+  props: {
+    previewList: {
+      type: Array,
+      default: () => {}
+    },
+    src: {
+      type: String,
+      dafault: ""
+    }
+  },
+  created() {
+    if (this.previewList.includes(this.src)) {
+      this.index = this.previewList.indexOf(this.src);
+    }
+  },
+  data() {
+    return {
+      index: 0
+    };
+  }
+};
+</script>
+
+<style>
+@import "../../../src/font/iconfont.css";
+</style>
--- packages/image/src/main.vue
+++ packages/image/src/main.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="nb-image">
-    <img
-      src="https://dbsys.obs.cn-east-3.myhuaweicloud.com/dbsys/dev/db/other/20200819/0d53ac1a0d13434791102d04da50a0ae.png"
-      alt="test"
-    />
+  <div
+    class="nb-image"
+    :class="[ circle ? 'nb-image--circle' : '']"
+    style="width: 100px; height: 100px"
+  >
+    <img :src="src" alt="test" :style="imgStyle" class="nb-image__inner" />
+    <image-viewer
+      :previewList="['https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg']"
+    ></image-viewer>
   </div>
 </template>
 
 <script>
+import ImageViewer from "./image-preview";
 export default {
   name: "NbImage",
-  prop: {
-    type: "" //图片适应方式
+  components: { ImageViewer },
+  props: {
+    src: {
+      type: String,
+      default:
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"
+    },
+    type: {
+      type: String,
+      default: "cover",
+      validator: t => {
+        const acceptTypes = ["fill", "contain", "cover", "none", "scale-down"];
+        return acceptTypes.includes(t);
+      }
+    }, //图片适应方式
+    circle: {
+      type: Boolean,
+      default: false
+    },
+    previewList: {
+      type: Array,
+      default: () => {}
+    }
   },
   computed: {
     imgStyle() {
+      console.log(this.type);
       let style = {
         "object-fit": this.type
       };
+      return style;
     }
   },
   data() {
@@ -26,8 +54,5 @@ export default {
 };
 </script>
 
-<style>
-img {
-  border-radius: 50%;
-}
+<style lang="scss" src="../../../theme/image.scss">
 </style>
